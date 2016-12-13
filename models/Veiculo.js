2b46cce6afc743606888487987e0a9f7097f9d40
Hugo Galvão Ribeiro Arraes <hugograck@gmail.com>
2016-12-13 11:19:01 -0200

    models Veiculo

---
+++ models/Veiculo.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var VeiculoSchema = new mongoose.Schema({
+  modelo: String,
+  cor: String,
+  chassi: Number,
+  marca: String,
+  preco: Number,
+  popularidade: Number
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
